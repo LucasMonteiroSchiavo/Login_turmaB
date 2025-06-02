05f6dc97f8ee93f4b003ea67195b53fdb523a9c1
Your Name <you@example.com>
2025-06-02 20:25:25 -0300

    02/06
    xD

--- js/login.js
+++ js/login.js
@@ -1,1 +1,22 @@
 // login com validação
+const entrar = document.getElementById('entrar');
+
+entrar.addEventListener('click', function() {
+  let login = document.getElementById('login').value;
+  let senha = document.getElementById('senha').value;
+
+  // Corrigido: JSON.parse + fallback para array vazio
+  const usuarioCadastrados = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+  let usuarioEncontrado = usuarioCadastrados.find(
+    u => u.usuario === login && u.senha === senha
+  );
+if(usuarioEncontrado){
+    localStorage.setItem("userLogado", login);
+    window.location.href="dashboard.html";
+}else{
+    document.getElementById('mensagem').textContent = "Login ou senha incorretos";
+}
+  console.log(usuarioEncontrado);
+}
+);
